8fe636bc456c9005a75e1ba0d568ee458a89c6cc
Vincent FRICOU <vincent+github@fricouv.eu>
2018-05-28 10:11:31 +0200

    Adding sleep to wait log file created

--- includes/entrypoint.sh
+++ includes/entrypoint.sh
@@ -6,4 +6,5 @@ fi
 
 chown -R nginx:nginx /privatebin
 supervisord -c /usr/local/etc/supervisord.conf &
+sleep 10
 tail -f /supervisord.log /var/log/nginx/privatebin_access.log /var/log/nginx/privatebin_error.log /var/log/nginx/privatebin.access.log
