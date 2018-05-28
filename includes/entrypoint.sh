8a796fa70823e41490b503a34f9b257000f8fd8e
Vincent FRICOU <vincent+github@fricouv.eu>
2018-05-28 10:13:41 +0200

    Switch log displaying into supervisord

--- includes/entrypoint.sh
+++ includes/entrypoint.sh
@@ -5,6 +5,4 @@ if [ ! -f ${PB_ROOT_DIR}/cfg/conf.php ]; then
 fi
 
 chown -R nginx:nginx /privatebin
-supervisord -c /usr/local/etc/supervisord.conf &
-sleep 10
-tail -f /supervisord.log /var/log/nginx/privatebin_access.log /var/log/nginx/privatebin_error.log /var/log/nginx/privatebin.access.log
+supervisord -c /usr/local/etc/supervisord.conf
